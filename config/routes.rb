df2db5994e768d258489b4b1571603d6607ccb33
EC2 Default User <ec2-user@ip-172-31-12-200.ap-northeast-1.compute.internal>
2024-08-15 19:16:57 +0000

    [Add]bootstrap導入

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   get 'homes/top'
   get 'homes/about'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
