00661ef149f4c9bd6e358a1041d489334eaa73ed
EC2 Default User <ec2-user@ip-172-31-12-200.ap-northeast-1.compute.internal>
2024-08-17 20:53:14 +0000

    [Update]Sign upページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :edit]
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
-  devise_for :users
 end
