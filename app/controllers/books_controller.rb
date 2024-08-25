a8278d55afd4479b1c0634bb8daa2fad4d2ad825
EC2 Default User <ec2-user@ip-172-31-12-200.ap-northeast-1.compute.internal>
2024-08-25 14:43:55 +0000

    [Update]投稿更新フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
   end
   
